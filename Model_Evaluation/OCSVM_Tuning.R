a553cb11ea489a5ba7b633f3a0fce839660bc229
isaacbaguisa <isaac.baguisa@mail.utoronto.ca>
2025-03-25 21:56:08 -0400

    Improve tuning method for nu and gamma hyperparameters and threshold

--- Model_Evaluation/OCSVM_Tuning.R
+++ Model_Evaluation/OCSVM_Tuning.R
@@ -2,8 +2,8 @@
 # to optimize anomaly detection performance.
 
 # Define possible values for nu and gamma
-nu_values <- seq(0.05, 0.5, by = 0.05)  # Range of nu values
-gamma_values <- c(0.01, 0.05, 0.1, 0.5)  # Range of gamma values
+nu_values <- seq(0.01, 0.5, by = 0.05)  # Range of nu values
+gamma_values <- c(0.01, 0.05, 0.1, 0.5, 1)  # Range of gamma values
 
 # Initialize a data frame to store results
 results <- data.frame(nu = numeric(), gamma = numeric(), accuracy = numeric())
@@ -13,19 +13,16 @@ for (nu in nu_values) {
   for (gamma in gamma_values) {
     
     # Train the OCSVM model
-    ocsvmModel <- svm(s1$pdcsap_flux_imputed, type = "one-classification", 
-                      nu = nu, kernel = "radial", scale = TRUE, gamma = gamma, 
-                      decision.values = TRUE)
+    ocsvmModel <- svm(s1$pdcsap_flux_imputed, type = "one-classification", nu = nu,
+                      kernel = "radial", scale = TRUE, 
+                      gamma = gamma, decision.values = TRUE)
     
-    # Get the decision function scores
-    decisionScores <- ocsvmModel$decision.values
-    
-    # Set threshold based on quantiles or another method
-    threshold <- quantile(decisionScores, 0.90)
-    #threshold <- -1000
+    # Predict using the trained OCSVM model on the simulated data for Star 3
+    ocsvmPred <- predict(ocsvmModel, 
+                         newdata = data.frame(simulatedS1$pdcsap_flux_imputed))
     
     # Predict anomalies based on the threshold
-    predicted_flare_ocsvm <- ifelse(decisionScores < threshold, 1, 0)
+    predicted_flare_ocsvm <-  ifelse(ocsvmPred, 0, 1)
     
     # Calculate accuracy (or other metrics)
     accuracy <- mean(predicted_flare_ocsvm == simulatedS1$flare_label)
@@ -41,3 +38,4 @@ cat("Best parameters: nu =", best_params$nu, "gamma =", best_params$gamma, "\n")
 cat("Best accuracy: ", best_params$accuracy, "\n")
 
 #Best parameters: nu = 0.05 gamma = 0.01 
+
